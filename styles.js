c8ff8cceef3e82d2b26c7ebdf8b4604d79fa288d
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-03-04 19:46:36 +0100

    initial commit

--- styles.js
+++ styles.js
@@ -1,14 +1,24 @@
 import { createGlobalStyle } from "styled-components";
+import { Moirai_One } from "next/font/google";
+
+const moiraiOne = Moirai_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default createGlobalStyle`
   *,
   *::before,
   *::after {
     box-sizing: border-box;
+ 
   }
 
   body {
-    margin: 0;
-    font-family: system-ui;
+    margin: 0 auto;
+    max-width: 480px;
+    font-family: ${moiraiOne.style.fontFamily};
+    padding: 8px;
+    background-color: #e2caed;
   }
 `;
